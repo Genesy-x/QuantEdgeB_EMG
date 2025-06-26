434a62cd81e09f9d0ff6d689fbbf852ea029169d
Genesy-x <contact@aureus-solutions.com>
2025-06-26 10:37:53 +0200

    Updated PremiumPlanPage.tsx

--- src/pages/PremiumPlanPage.tsx
+++ src/pages/PremiumPlanPage.tsx
@@ -12,7 +12,7 @@ function PremiumPlanPage() {
         <>
         Built to track macro-to-micro dynamics and help you act with clarity—not emotion.
 </>,
-      image: "https://res.cloudinary.com/dq4flimtm/image/upload/v1749630079/3_chpith.webp",
+      image: "https://res.cloudinary.com/dq4flimtm/image/upload/v1749059082/QuantEdgeB_Crypto_Trading_Uni_System.png",
       features: [
         <>
         Includes everything in&nbsp; <em> <strong> Fundamental Tier </strong> </em>
