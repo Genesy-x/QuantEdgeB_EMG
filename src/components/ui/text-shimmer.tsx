f41b19da069539287040d35b8c7536731883b305
Genesy-x <contact@aureus-solutions.com>
2025-06-27 10:52:00 +0200

    Updated text-shimmer.tsx

--- src/components/ui/text-shimmer.tsx
+++ src/components/ui/text-shimmer.tsx
@@ -30,7 +30,7 @@ export function TextShimmer({
         'relative inline-block bg-[length:250%_100%,auto] bg-clip-text',
         'text-transparent [--base-color:#a1a1aa] [--base-gradient-color:#000]',
         '[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))] [background-repeat:no-repeat,padding-box]',
-        'dark:[--base-color:#0924d6] dark:[--base-gradient-color:#ffffff] dark:[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))]',
+        'dark:[--base-color:theme(colors.blue.300)] dark:[--base-gradient-color:#ffffff] dark:[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))]',
         className
       )}
       initial={{ backgroundPosition: '100% center' }}
