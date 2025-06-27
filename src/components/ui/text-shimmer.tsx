1a7ec7ceb129ba62c3d7f109db2a9f762b59e28e
Genesy-x <contact@aureus-solutions.com>
2025-06-27 10:45:35 +0200

    Updated text-shimmer.tsx

--- src/components/ui/text-shimmer.tsx
+++ src/components/ui/text-shimmer.tsx
@@ -26,13 +26,14 @@ export function TextShimmer({
 
   return (
     <MotionComponent
-      className={cn(
-        'relative inline-block bg-[length:250%_100%,auto] bg-clip-text',
-        'text-transparent [--base-color:#a1a10916d6aa] [--base-gradient-color:#000]',
-        '[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))] [background-repeat:no-repeat,padding-box]',
-        'dark:[--base-color:#0916d6] dark:[--base-gradient-color:#ffffff] dark:[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))]',
-        className
-      )}
+  className={cn(
+    'relative inline-block bg-[length:250%_100%,auto] bg-clip-text text-transparent',
+    '[--base-color:theme(colors.blue.400)] [--base-gradient-color:theme(colors.blue.800)]',
+    '[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))] [background-repeat:no-repeat,padding-box]',
+    'dark:[--base-color:theme(colors.blue.300)] dark:[--base-gradient-color:theme(colors.white)]',
+    'dark:[--bg:linear-gradient(90deg,#0000_calc(50%-var(--spread)),var(--base-gradient-color),#0000_calc(50%+var(--spread)))]',
+    className
+  )}
       initial={{ backgroundPosition: '100% center' }}
       animate={{ backgroundPosition: '0% center' }}
       transition={{
